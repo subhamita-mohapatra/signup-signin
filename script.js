a496a7ad3cbbc5a8f35e6b72758840446a83c88e
subhamita-mohapatra <personalaccsubha@gmaiil.com>
2026-03-19 19:42:39 +0530

    message

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSignup() {
+    document.getElementById("signin").classList.add("hidden");
+    document.getElementById("signup").classList.remove("hidden");
+}
+
+function showSignin() {
+    document.getElementById("signup").classList.add("hidden");
+    document.getElementById("signin").classList.remove("hidden");
+}
